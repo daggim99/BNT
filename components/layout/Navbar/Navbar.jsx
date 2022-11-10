3f45f8276584836d779835f9dc31066983458d2b
daggim99 <programmerdaggim99@gmail.com>
2022-11-10 19:21:32 +0300

    fix mobile navbar issue

--- components/layout/Navbar/Navbar.jsx
+++ components/layout/Navbar/Navbar.jsx
@@ -104,7 +104,7 @@ export default function Navbar(index) {
             <div className="space-y-1 px-2 pt-2 pb-3 text-white">
               {navigation.map((item, inx) => (
                 <Disclosure.Button
-                  as="a"
+                  as="Link"
                   key={item.name}
                   href={item.href}
                   onClick={(e) => handleActiveLink(inx)}
