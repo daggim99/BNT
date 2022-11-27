0ddec13e293147adc692dac7e5e04a2eb98db685
Daggim 99 <programmerdaggim99@gmail.com>
2022-11-27 17:46:16 +0300

    fix section spacing

--- components/layout/Section/OurProduct/OurProduct.jsx
+++ components/layout/Section/OurProduct/OurProduct.jsx
@@ -5,7 +5,7 @@ const OurProducts = () => (
     title="Our Brands"
     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis."
     className={{
-      card: 'max-h-[100vh] min-h-[50vh] overflow-scroll md:overflow-clip sm:max-h-[65vh] sm:min-h-[50vh] pt-9 md:pt-0 md:h-[45vh] lg:h-[40vh] row-start-2 row-end-3 py-9 dark:bg-white',
+      card: 'lg:pt-12 max-h-[100vh] min-h-[50vh] overflow-scroll md:overflow-clip sm:max-h-[65vh] sm:min-h-[60vh] pt-9 md:pt-0 md:h-[45vh] lg:h-[50vh] row-start-2 row-end-3 py-9 dark:bg-white',
       cardTitle:
         'justify-self-center align-bottom self-center text-center md:mt-24',
       cardText:
