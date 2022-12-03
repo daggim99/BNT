cb539d4769b0781c4d2a20ad883ff97f0e8fbbdf
Daggim 99 <programmerdaggim99@gmail.com>
2022-12-03 12:47:55 +0300

    fix hieght of the section component

--- app/about/Section.Component.jsx
+++ app/about/Section.Component.jsx
@@ -10,7 +10,7 @@ const Section = (props) => {
   const { left, title } = props
 
   return (
-    <section className="md:mt-3 overflow-x-hidden lg:overflow-clip h-[70vh] md:h-[60vh] rounded-none w-full overflow-scroll md:overflow-visible m-0 md:w-[80vw] md:m-auto grid grid-cols-[1fr] md:grid-cols-[1fr_1fr] grid-rows-[1fr_1fr] md:grid-rows-1 gap-y-24 md:gap-y-0 gap-x-12">
+    <section className="mt-16 mb-16 overflow-x-hidden lg:overflow-clip h-[70vh] md:h-[60vh] lg:h-[377px] rounded-none w-full overflow-scroll md:overflow-visible m-0 md:w-[80vw] md:m-auto grid grid-cols-[1fr] md:grid-cols-[1fr_1fr] grid-rows-[1fr_1fr] md:grid-rows-[377px] gap-y-24 md:gap-y-0 gap-x-12">
       <motion.div
         initial={{ opacity: 0, scale: 0, x: 14 }}
         whileInView={{ opacity: 1, scale: 1, x: 0 }}
@@ -25,13 +25,13 @@ const Section = (props) => {
           height={100}
           src={'/images/About/imageHigh.png'}
           alt={'image'}
-          className="w-full object-cover filter brightness-50 -z-50 object-center md:object-contain h-full"
+          className="w-full object-cover filter brightness-50 md:brightness-100 -z-50 object-center md:object-contain h-full"
         />
       </motion.div>
       <div
         className={`col-start-1 col-span-full md:col-start-${
           left ? '1' : '2'
-        } filter brightness-200 self-center col-span-1 row-span-full grid grid-cols-[1fr] grid-rows-[min-content_min-content_min-content] gap-y-3`}
+        } filter brightness-200 self-center col-span-1 row-span-full grid grid-cols-[1fr] grid-rows-[81px_172px_59.96px] gap-y-3`}
       >
         <motion.div
           initial={{ opacity: 0, scale: 0, x: 14 }}
@@ -62,7 +62,7 @@ const Section = (props) => {
           className={`w-full md:w-[608px]`}
         >
           <p
-            className={`text-[#6D6D6D] px-6 md:py-6 lg:py-9 xl:py-16 2xl:py:24 md:px-0 text-[18px] md:text-[20px] font-roboto leading-[34.84px]`}
+            className={`text-[#6D6D6D] md:px-0 text-[18px] md:text-[20px] font-roboto leading-[34.84px]`}
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
             nisl eros, pulvinar facilisis justo mollis, auctor consequat urna.
